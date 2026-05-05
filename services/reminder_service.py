707a8522bbc8c0af4d776c18382c92f0bdab085e
Luto228 <megapupsik909@gmail.com>
2026-05-05 23:04:42 +0400

    feat: first part of 'reminder'

---
+++ services/reminder_service.py
@@ -0,0 +1,23 @@
+import SQlite3
+
+db = sqlite3.connect('reminder.db')
+cursor = db.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS reminders (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    time TEXT NOT NULL,
+    task TEXT NOT NULL,
+    status TEXT DEFAULT 'pending'
+    ''')
+
+def add_reminder(user_id, time, task):
+    cursor.execute("INSERT INTO reminders (user_id, time, task) VALUES (?, ?, ?)", (user_id, time, task))
+    db.commit()
+def get_status():
+    cursor.execute("SELECT * FROM reminders WHERE status = 'pending'")
+    fetchall_pending = cursor.fetchall()
+    return fetchall_pending
+def update_status(id):
+    cursor.execute("UPDATE reminders SET status = 'done' WHERE id = ?", (id,))
